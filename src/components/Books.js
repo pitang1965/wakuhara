f3b071a4f1ef1728de1e460ebc6d2705af89c154
Shoji Makino <pitang1965@gmail.com>
2021-08-21 23:19:17 +0900

    Fix: The same book exists

--- src/components/Books.js
+++ src/components/Books.js
@@ -26,18 +26,6 @@ const Books = () => {
         <Book
           code={`
             <iframe
-            style="width:120px;height:240px;"
-            marginwidth="0"
-            marginheight="0"
-            scrolling="no"
-            frameborder="0"
-            src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=pitang1965-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=4908925763&linkId=40e74400c69a2d363ae23d78f8996f89"
-            ></iframe>
-        `}
-        />
-        <Book
-          code={`
-            <iframe
               style="width:120px;height:240px;"
               marginwidth="0"
               marginheight="0"
